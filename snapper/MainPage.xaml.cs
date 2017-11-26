307589f5f5cc99534023269d29b4b93d10baded3
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-11-25 18:39:22 -0600

    Add sub pages

--- snapper/MainPage.xaml.cs
+++ snapper/MainPage.xaml.cs
@@ -1,30 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace snapper
-{
+namespace snapper {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
+    public sealed partial class MainPage {
+        public MainPage() {
             this.InitializeComponent();
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e) {
+            foreach (NavigationViewItemBase item in NavView.MenuItems) {
+                if (item is NavigationViewItem && item.Tag.ToString() == "edit") {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args) {
+            if (args.IsSettingsInvoked) {
+                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else {
+                switch (args.InvokedItem) {
+                    case "Home":
+                        ContentFrame.Navigate(typeof(HomePage));
+                        break;
+                    case "Edit":
+                        ContentFrame.Navigate(typeof(EditPage));
+                        break;
+                    case "About":
+                        ContentFrame.Navigate(typeof(AboutPage));
+                        break;
+                }
+            }
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args) {
+            if (args.IsSettingsSelected) {
+                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+
+                switch (item.Tag) {
+                    case "home":
+                        ContentFrame.Navigate(typeof(HomePage));
+                        break;
+                    case "edit":
+                        ContentFrame.Navigate(typeof(EditPage));
+                        break;
+                    case "about":
+                        ContentFrame.Navigate(typeof(AboutPage));
+                        break;
+                }
+            }
         }
     }
 }
