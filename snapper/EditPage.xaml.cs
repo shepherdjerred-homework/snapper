20b7418ad7edb99204d96e270bf2d69e9dc261a8
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-11-25 19:02:49 -0600

    Fix layout

--- snapper/EditPage.xaml.cs
+++ snapper/EditPage.xaml.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+using Windows.UI.Xaml.Controls;
 
 namespace snapper {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class EditPage : Page {
+    public sealed partial class EditPage {
         public EditPage() {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            ItemCollection items = snap_list.Items;
+            items.Add("Snap 1");
+            items.Add("Snap 2");
+            items.Add("Snap 3");
+            items.Add("Snap 4");
+            items.Add("Snap 5");
         }
     }
 }
--- snapper/MainPage.xaml.cs
+++ snapper/MainPage.xaml.cs
@@ -1,15 +1,10 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace snapper {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage {
         public MainPage() {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         private void NavView_Loaded(object sender, RoutedEventArgs e) {
